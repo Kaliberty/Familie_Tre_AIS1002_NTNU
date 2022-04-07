451a0a19dc69adba5bc715b0ad01789b8a1fd483
Jon-Fredrik Henning <jon_fredrik_henning@hotmail.com>
2022-04-07 22:51:55 +0200

    Updated a few classes

--- src/person.cpp
+++ src/person.cpp
@@ -1,1 +1,12 @@
 #include "person.hpp"
+
+
+Person::Person(const std::string &name) : name_(name) {}
+
+const std::string &Person::getName() const {
+    return name_;
+}
+
+void Person::setName(const std::string &name) {
+    name_ = name;
+}
