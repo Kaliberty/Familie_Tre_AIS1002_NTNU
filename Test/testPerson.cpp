bad573cdb2cd95f972d9f92d07b3d9552c25719e
Emil <emilemil09@gmail.com>
2022-05-16 16:30:00 +0200

    Person Setter Test Update

--- Test/testPerson.cpp
+++ Test/testPerson.cpp
@@ -6,14 +6,31 @@
 
 
     TEST_CASE("Test Getters") {
-        std::string firstName_ = John;
-        std::string lastName_ = Henning;
-        int age = 26;
-        bool gender = 0;
 
-        Person person(firstName_);
+
+        Person person();
 }
 
     TEST_CASE("Test Setters") {
+        std::string firstName_ = "John";
+        std::string lastName_ = "Henning";
+        int age_ = 26;
+        bool gender_ = 0;
+        Person person;
 
-}
+        REQUIRE("" == Approx(person.getFirstName()));
+        REQUIRE("" == Approx(person.getLastName()));
+        REQUIRE(0 == Approx(person.getAge()));
+        REQUIRE(0 == Approx(person.getGender()));
+
+        person.setFirstName(firstName_);
+        person.setLastName(lastName_);
+        person.setAge(age_);
+        person.setGender(gender_);
+
+        REQUIRE(firstName_ == person.getFirstName);
+        REQUIRE(lastName_ == person.getLastName);
+        REQUIRE(age_ == person.getAge);
+        REQUIRE(gender_ == person.getGender);
+
+}'
