014d2d82b2a3cbf5cb77882d237913583d5212d1
Jon-Fredrik Henning <jon_fredrik_henning@hotmail.com>
2022-05-20 06:24:16 +0200

    currently working + add node

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,10 @@
 #include "FamilyTree.hpp"
 
 
-int main(){
+int main() {
     Person familyName;
 
- //   f.setAge(11);
+    //
     familyName.setAge(12);
 //   Node test(p);
 //   Node left(f);
@@ -17,28 +17,49 @@ int main(){
     familyName.setFirstName("test");
     auto myTree = std::make_shared<FamilyTree>(familyName);
     Person f;
+    Person z;
+    f.setAge(11);
+    z.setAge(10);
     myTree->addNode(f);
+    myTree->addNode(z);
 
-    std::cout << myTree->getCurrentData().getAge() << std::endl;
-    //myTree->getCurrentNode()->
 
-   // std::cout << myTree->getCurrentNode().isRoot << std::endl;
-   // std::cout << myTree->getChildrenData() << std::endl;
+  //  std::cout << myTree->getCurrentData().getAge() << std::endl;
+
+  /*  myTree->traverseDepthFirst([&](std::shared_ptr<FamilyTree::Node> f){
+        myTree->getSearchList().emplace_back(f);
+    }); */
+
+
+
+    /*for(const auto& x : myTree->getSearchList()){
+        std::cout << x->getData().getAge() << std::endl;
+    }*/
+
+     //myTree->traverseDepthFirst();
+    //std::cout << myTree->getSearchList().size();
+    std::cout << myTree->getCurrentChildren()[1]->getData().getAge();
+
+    //std::cout << myTree->getCurrentChildren().getAge();
+    // myTree->getCurrentNode().getAge();
+
+    // std::cout << myTree->getCurrentNode().isRoot << std::endl;
+    // std::cout << myTree->getChildrenData() << std::endl;
 
     // auto myTree = std::make_shared<FamilyTree>(familyName);
- //   test.add(left);
+    //   test.add(left);
 
 
-   // std::cout << p.getFirstName() << std::endl;
-  //  std::cout << p.getAge() << std::endl;
-  //  std::cout << f.getAge() << std::endl;
- //   std::cout << test.getData().getAge() << std::endl;
+    // std::cout << p.getFirstName() << std::endl;
+    //  std::cout << p.getAge() << std::endl;
+    //  std::cout << f.getAge() << std::endl;
+    //   std::cout << test.getData().getAge() << std::endl;
     //std::cout << test->getAge();
 //    std::cout << test.isRoot() << std::endl;
 
     //std::cout << test.parent_ << std::endl;
 
- //   std::make_shared<d>()
+    //   std::make_shared<d>()
 
 
 
--- include/family-tree/Tree.hpp
+++ include/family-tree/Tree.hpp
@@ -4,27 +4,35 @@
 #include <utility>
 #include <vector>
 #include <ostream>
+#include <functional>
 
 //#include <string>
 
 
 template<class T>
 class Tree {
-
+public:
     class Node {
     public:
-        explicit Node(T& data) {
+        explicit Node(T &data) {
             data_ = data;
         };
-        auto getData() { return data_;}
-        void setData(T data) {data_ = data;}
 
-        [[nodiscard]] bool isRoot() const {return {parent_ == nullptr};}
-        [[nodiscard]] bool isLeaf() const {return children_.empty();}
-        [[nodiscard]] bool hasParent() const {return parent_ != nullptr;}
-        auto &getChildren() {return children_;}
-        auto &getParent() {return parent_;}
-       // friend std::ostream &operator<<(std::ostream &os, Node &n){os << *n.data_; return os;}
+        auto &getData() { return data_; }
+
+        void setData(T data) { data_ = data; }
+
+        [[nodiscard]] bool isRoot() const { return {parent_ == nullptr}; }
+
+        [[nodiscard]] bool isLeaf() const { return children_.empty(); }
+
+        [[nodiscard]] bool hasParent() const { return parent_ != nullptr; }
+
+        auto &getChildren() { return children_; }
+
+        auto &getParent() { return parent_; }
+
+        // friend std::ostream &operator<<(std::ostream &os, Node &n){os << *n.data_; return os;}
     private:
         T data_;
         std::vector<std::shared_ptr<Node>> children_;
@@ -32,27 +40,57 @@ class Tree {
     };
 
 
-public:
     Tree(T &data) {
         auto rootNode = std::make_shared<Node>(data);
         root_ = rootNode;
         currentNode_ = root_;
     }
-    T &getCurrentNode() const{return currentNode_;}
-    T &getCurrentData() const{return currentNode_->getData();}
-    T &getCurrentChildren() {return currentNode_->getChildren();}
+
+    auto getCurrentNode() const { return currentNode_; }
+
+    T &getCurrentData() const { return currentNode_->getData(); }
+
+    auto getCurrentChildren() { return currentNode_->getChildren(); }
+
+    auto getCurrentParent() { return currentNode_->getParent(); }
+
+    std::shared_ptr<Node> getRoot() const { return root_; }
+
+    auto getSearchList() { return searchList_; }
 
     void addNode(T node) {
         auto addedNode = std::make_shared<Node>(node);
-       // node.parent_ = this; // this -> Node*
-       currentNode_->getChildren().push_back(node);
-       addedNode->getParent() = currentNode_;
+        currentNode_->getChildren().push_back(addedNode);
+        addedNode->getParent() = currentNode_;
+    }
+/*
+    void traverseDepthFirst(const std::function<bool(std::shared_ptr<Node>)> &f){
+        searchList_.clear();
+        if(f(getRoot())){
+            searchList_.push_back(getRoot());
+        }
+        traverseDepthFirstHelper(getRoot(),f);
 
+    }
+    void traverseDepthFirstHelper(std::shared_ptr<Node> n, const std::function<bool(std::shared_ptr<Node>)> &f){
+        for (auto c: n->getChildren()){
+            if(f(c)){
+                searchList_.push_back(c);
+            }
+            traverseDepthFirstHelper(c,f);
+        }
+    }*/
+    void traverseDepthFirst(const std::function<void(std::shared_ptr<Node>)> &f) {
+        f(getRoot());
+        for (auto c: getCurrentChildren()) {
+            c->traverseDepthFirst(f);
+        }
     }
 
 private:
     std::shared_ptr<Node> root_;
     std::shared_ptr<Node> currentNode_;
+    std::vector<std::shared_ptr<Node>> searchList_;
 };
 
 
