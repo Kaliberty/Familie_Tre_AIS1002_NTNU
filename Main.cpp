1dfa9a1d60bca2b7029026e986f23402a0a68832
Jon-Fredrik Henning <jon_fredrik_henning@hotmail.com>
2022-05-20 06:46:49 +0200

    Compiles

--- Main.cpp
+++ Main.cpp
@@ -12,7 +12,7 @@ public:
 };
 
 
-bool running = true;
+bool running = false;
 int main() {
     Person familyName;
 
--- include/family-tree/FamilyTree.hpp
+++ include/family-tree/FamilyTree.hpp
@@ -5,7 +5,10 @@
 #include "Tree.hpp"
 
 // FamilyTree.hpp is used to populate the template with a Person
-class FamilyTree : public Tree<Person>{};
+class FamilyTree : public Tree<Person>{
+public:
+    using Tree<Person>::Tree;
+};
 
 
 #endif //FAMILY_TREE_FAMILYTREE_HPP
